609e7040c5c5e6b04acb95fbf22d4406b75d9825
Claudio Kuenzler <ck@claudiokuenzler.com>
2019-07-08 14:42:01 +0200

    Handle different Windows languages

--- check_win_net_usage.sh
+++ check_win_net_usage.sh
@@ -7,7 +7,25 @@
 # Unfortunately this is necessary because the Windows perfomance output
 # has two separate entries for Received and Sent Bytes/s.
 #
-# Author:       Claudio Kuenzler www.claudiokuenzler.com
+# Official doc: https://www.claudiokuenzler.com/monitoring-plugins/check_win_net_usage.php
+#
+# License:      GPLv2                                                          
+# GNU General Public Licence (GPL) http://www.gnu.org/ 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <https://www.gnu.org/licenses/>. 
+#
+# Copyright 2011,2012,2015,2019 Claudio Kuenzler www.claudiokuenzler.com
+# 
 # History:
 # 20111116      First version released
 # 20120126      Bugfix in port check
@@ -17,12 +35,13 @@
 # 20151127      Handling connection error on second connection, too
 # 20151127      Fix perfdata format
 # 20151215      Add network interface detection (-d parameter))
+# 20190708	Handle different Windows languages (only 'de' for now)
 #############################################################################
 # Set path to the location of your Nagios plugin (check_nt)
 pluginlocation="/usr/lib/nagios/plugins"
 #############################################################################
 # Help
-help="check_win_net_usage.sh (c) 2011-2015 Claudio Kuenzler (GPLv2)\n
+help="check_win_net_usage.sh (c) 2011-2019 Claudio Kuenzler (GPLv2)\n
 Usage: ./check_win_net_usage.sh -H host [-p port] [-s password] -i intname
 Requirements: check_nt plugin and NSClient++ installed on target server
 \nOptions:\n-H Hostname of Windows server to check
@@ -46,7 +65,7 @@ echo "CRITICAL - Plugin check_nt not found in ${pluginlocation}"; exit 2
 fi
 #############################################################################
 # Get user-given variables
-while getopts "H:p:s:i:o:d" Input;
+while getopts "H:p:s:i:o:dl:" Input;
 do
        case ${Input} in
        H)      host=${OPTARG};;
@@ -55,6 +74,7 @@ do
        i)      interface=${OPTARG};;
        o)      output=${OPTARG};;
        d)      detect=1;;
+       l)      winlang=${OPTARG};;
        *)      echo "Wrong option given. Please rtfm or launch --help"
                exit 1
                ;;
@@ -66,13 +86,19 @@ if [[ -n ${port} ]]
 then insertport=${port}
 else insertport=12489
 fi
+#############################################################################
+# Handle different languages (might switch to case in future)
+if [[ $winlang = "de" ]]; then interfacename="Netzwerkadapter"
+else interfacename="Network Interface"
+fi
+
 
 # If -d (detection) is used, present list of interface names
 if [[ ${detect} -eq 1 ]]; then
   if [[ -n ${password} ]]; then
-    ${pluginlocation}/check_nt -H ${host} -p ${insertport} -s ${password} -v INSTANCES -l "Network Interface" | sed "s/OK&//"; exit 0
+    ${pluginlocation}/check_nt -H ${host} -p ${insertport} -s ${password} -v INSTANCES -l "${interfacename}" | sed "s/OK&//"; exit 0
   else
-    ${pluginlocation}/check_nt -H ${host} -p ${insertport} -v INSTANCES -l "Network Interface" | sed "s/OK&//"; exit 0
+    ${pluginlocation}/check_nt -H ${host} -p ${insertport} -v INSTANCES -l "${interfacename}" | sed "s/OK&//"; exit 0
   fi
 fi
 
@@ -82,12 +108,12 @@ if [[ -z ${interface} ]]; then echo "UNKNOWN - No interface given"; exit 3; fi
 # The checks itself (with password)
 if [[ -n ${password} ]]
 then
-bytes_in=$(${pluginlocation}/check_nt -H ${host} -p ${insertport} -s ${password} -v COUNTER -l "\\Network Interface(${interface})\\Bytes Received/sec")
-bytes_out=$(${pluginlocation}/check_nt -H ${host} -p ${insertport} -s ${password} -v COUNTER -l "\\Network Interface(${interface})\\Bytes Sent/sec")
+bytes_in=$(${pluginlocation}/check_nt -H ${host} -p ${insertport} -s ${password} -v COUNTER -l "\\${interfacename}(${interface})\\Bytes Received/sec")
+bytes_out=$(${pluginlocation}/check_nt -H ${host} -p ${insertport} -s ${password} -v COUNTER -l "\\${interfacename}(${interface})\\Bytes Sent/sec")
 else
 # Without password
-bytes_in=$(${pluginlocation}/check_nt -H ${host} -p ${insertport} -v COUNTER -l "\\Network Interface(${interface})\\Bytes Received/sec")
-bytes_out=$(${pluginlocation}/check_nt -H ${host} -p ${insertport} -v COUNTER -l "\\Network Interface(${interface})\\Bytes Sent/sec")
+bytes_in=$(${pluginlocation}/check_nt -H ${host} -p ${insertport} -v COUNTER -l "\\${interfacename}(${interface})\\Bytes Received/sec")
+bytes_out=$(${pluginlocation}/check_nt -H ${host} -p ${insertport} -v COUNTER -l "\\${interfacename}(${interface})\\Bytes Sent/sec")
 fi
 
 # Catch connection error
